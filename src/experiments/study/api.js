6416f685e764bc9db6394e8b185e47e34b7fa5e0
Kevin Jacobs <kjacobs@mozilla.com>
2019-10-28 15:43:44 -0700

    Fix issues 1, 2. Add prefs to remember last setting and only run the experiment once

--- src/experiments/study/api.js
+++ src/experiments/study/api.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /* exported study */
 /* global Cc, Ci, Components, ExtensionAPI, Services  */
 
@@ -9,6 +10,8 @@ Cu.importGlobalProperties(["XMLHttpRequest"]);
 
 const kDelegatedCredentialsHost = "kc2kdm.com";
 const kDelegatedCredentialsPref = "security.tls.enable_delegated_credentials";
+
+
 const kResults = {
   SUCCESS: 0,
   TIMEOUT: 1,
@@ -24,11 +27,13 @@ const kResults = {
 const BRANCH_CONTROL = "control";
 const BRANCH_TREATMENT = "treatment";
 
+
 /* Prefs handlers */
 const prefManager = {
   prefHasUserValue(name) {
     return Services.prefs.prefHasUserValue(name);
   },
+
   getPref(name, value) {
     let type = Services.prefs.getPrefType(name);
     switch (type) {
@@ -42,15 +47,40 @@ const prefManager = {
       throw new Error("Unknown type");
     }
   },
+
   setBoolPref(name, value) {
     Services.prefs.setBoolPref(name, value);
-  }
+  },
+
+  rememberBoolPref(name) {
+    const kPrefPrefix = "dc-experiment.previous.";
+    let curMode = Services.prefs.getBoolPref(name);
+    // eslint-disable-next-line no-console
+    // console.log("Saving current " + name + " pref: " + curMode);
+    Services.prefs.setBoolPref(kPrefPrefix + name, curMode);
+  },
+
+  restoreBoolPref(name) {
+    const kPrefPrefix = "dc-experiment.previous.";
+    let prevMode = Services.prefs.getBoolPref(kPrefPrefix + name);
+    // eslint-disable-next-line no-console
+    // console.log("Restoring " + kPrefPrefix + name + ": " + prevMode);
+    Services.prefs.setBoolPref(name, prevMode);
+  },
 };
 
-
+/* Submit the result for telemetry, and return true if successful.
+ * If the telemetry submission was unsuccessful OR the result itself
+ * indicates that we should retry the experiment, return false. */
 function recordResult(result) {
+  if (result.status === 521 || !result.hasResult) {
+    // 521 result means we could reach CF, but CF could not reach the host. In this case,
+    // mark the experiment as not-run, allowing it run again.
+    return false;
+  }
   // eslint-disable-next-line no-console
-  console.log(result); //TODO: Do the telemetry thing...
+  console.log(result); //TODO: Do the telemetry submission...
+  return true;
 }
 
 /* Record one of the following for telemetry:
@@ -60,7 +90,7 @@ function recordResult(result) {
  * |timed_out|: Network timeout.
  * |dns_failure|: Failed to connect due to a DNS failure.
  * |network_failure|: Failed to connect due to a non-timeout, non-dns network error (connection reset, etc).
- * |insufficient_security|: The delegated credential did not provide high enough security.                            <======= TODO: DC-only, or all connections?
+ * |insufficient_security|: The delegated credential did not provide high enough security.
  * |incorrect_tls_version|: Connected successfully, but used TLS < 1.3.
 */
 function populateResult(channel, result) {
@@ -69,7 +99,7 @@ function populateResult(channel, result) {
     secInfo.QueryInterface(Ci.nsITransportSecurityInfo);
     let isSecure = (secInfo.securityState & Ci.nsIWebProgressListener.STATE_IS_SECURE) == Ci.nsIWebProgressListener.STATE_IS_SECURE;
 
-    if (result.status >= 400 && result.status <= 521) { //HTTP Error codes. Maybe we can do better...
+    if (result.status >= 400 && result.status <= 521) { // HTTP Error codes
       result.telemetryResult = kResults.NET_FAILURE;
     }
     else if (isSecure) {
@@ -83,45 +113,58 @@ function populateResult(channel, result) {
         result.telemetryResult = kResults.SUCCESS;
       }
     }
-    else if (secInfo.isDelegatedCredential) { // TODO: Is this insufficient_security for only DC connections, or all?
+    else if (secInfo.isDelegatedCredential) {
       result.telemetryResult = kResults.INSUFFICIENT_SECURITY;
     }
+
+    result.hasResult = true;
   }
 }
 
-function makeRequest(branch, originalPrefVal) {
+function finishExperiment(result) {
+  // Revert the DC setting
+  prefManager.restoreBoolPref(kDelegatedCredentialsPref);
+
+  if (result.hasResult && recordResult(result)) {
+    // Mark the experiment as completed.
+    //Services.prefs.setBoolPref("dc-experiment.hasRun", true);
+    return;
+  }
+}
+
+function makeRequest(branch) {
   var result = {
-    "url" : "https://" + kDelegatedCredentialsHost,
-    "branch" : branch
+    "branch" : branch,
+    "hasResult" : false // True when we have something worth reporting
   };
 
   var oReq = new XMLHttpRequest();
-  oReq.open("GET", "https://" + kDelegatedCredentialsHost);
+  oReq.open("HEAD", "https://" + kDelegatedCredentialsHost);
+  oReq.setRequestHeader("X-Firefox-Experiment", "Delegated Credentials Breakage #1; https://bugzilla.mozilla.org/show_bug.cgi?id=1582591");
   oReq.timeout = 30000;
   oReq.addEventListener("error", e => {
     e.target.QueryInterface(Ci.nsIHttpChannel);
     let channel = e.target.channel;
     let nsireq = channel.QueryInterface(Ci.nsIRequest);
-    result.error = nsireq ? nsireq.status : Cr.NS_ERROR_NOT_AVAILABLE;
+    result.nsiReqError = nsireq ? nsireq.status : Cr.NS_ERROR_NOT_AVAILABLE;
     populateResult(channel, result);
-    recordResult(result);
-    prefManager.setBoolPref(kDelegatedCredentialsPref, originalPrefVal);
+    finishExperiment(result);
   });
   oReq.addEventListener("load", e => {
     //e.target.QueryInterface(Ci.nsIHttpChannel);
     result.status = e.target.status;
-    result.error = Cr.NS_OK;
+    result.nsiReqError = Cr.NS_OK;
     populateResult(e.target.channel, result);
-    recordResult(result);
-    prefManager.setBoolPref(kDelegatedCredentialsPref, originalPrefVal);
+    finishExperiment(result);
   });
-  oReq.addEventListener("timeout", e => {
+  oReq.addEventListener("timeout", () => {
     result.telemetryResult = kResults.TIMEOUT;
-    recordResult(result);
-    prefManager.setBoolPref(kDelegatedCredentialsPref, originalPrefVal);
+    result.hasResult = true;
+    finishExperiment(result);
   });
-  oReq.addEventListener("abort", e => {
-    prefManager.setBoolPref(kDelegatedCredentialsPref, originalPrefVal);
+  oReq.addEventListener("abort", () => {
+    // !hasResult means we reattempt.
+    finishExperiment(result);
   });
 
   oReq.send();
@@ -129,16 +172,28 @@ function makeRequest(branch, originalPrefVal) {
 
 // Returns true iff this session will perform the test.
 function getEnrollmentStatus() {
+  let val = Services.prefs.getBoolPref("dc-experiment.hasRun", false);
+  if (val != null && val === true) {
+    // The user has already run this experiment.
+    return false;
+  }
+
+  //return Math.random() >= 0.02;
   return true; // TODO: Be more selective...
 }
 
 // Returns true iff the test is to be performed with DC enabled.
 function getDCTreatment() {
-  //return Math.random() >= 0.5;
-  return true; // TODO: Be more selective...
+  return Math.random() >= 0.5;
 }
 
 const studyManager = {
+  uninstall() {
+    // TODO: How can we cleanup? https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/management is unsupported
+    Services.prefs.clearUserPref("dc-experiment.hasRun");
+    Services.prefs.clearUserPref("dc-experiment.previous." + kDelegatedCredentialsPref);
+  },
+
   runTest() {
     // If the user has already changed the default setting or they are not randomly selected, return early.
     if (prefManager.prefHasUserValue(kDelegatedCredentialsPref) ||
@@ -146,19 +201,17 @@ const studyManager = {
       return;
     }
 
-    //TODO: How do we ensure the original setting is restored in the case of early shutdown/crash?
-    let originalPrefVal = prefManager.getPref(kDelegatedCredentialsPref);
+    prefManager.rememberBoolPref(kDelegatedCredentialsPref);
     let testBranch = getDCTreatment() ? BRANCH_TREATMENT : BRANCH_CONTROL;
 
     if (testBranch === BRANCH_TREATMENT) {
-      //TODO: Enable DC (properly...?)
       prefManager.setBoolPref(kDelegatedCredentialsPref, true);
     } else {
       prefManager.setBoolPref(kDelegatedCredentialsPref, false);
     }
 
     // Attempt the connection
-    makeRequest(testBranch, originalPrefVal);
+    makeRequest(testBranch);
   }
 };
 
@@ -170,6 +223,9 @@ var study = class study extends ExtensionAPI {
         study: {
           runTest() {
             studyManager.runTest();
+          },
+          uninstall() {
+            studyManager.uninstall();
           }
         },
       },
